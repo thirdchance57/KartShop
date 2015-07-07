6e0f4e3c6bd641d079f943c86fe68d798b404778
thirdchance57 <thirdchance57@hotmail.com>
2015-07-07 09:48:27 -0700

    social media, contact

--- both/routes.js
+++ both/routes.js
@@ -8,4 +8,14 @@ FlowRouter.route('/', {
     // }
 });
 
+FlowRouter.route('/contact', {
+    // subscriptions: function(params) {
+    //     console.log("subscribe and register this subscription as 'myPost'");
+    //     this.register('products', Meteor.subscribe('products'));
+    // },
+    action: function(params) {
+        console.log("Yeah! We are on the contact");
+    }
+});
+
 FlowRouter.notFound = FlowRouter.go('/');
